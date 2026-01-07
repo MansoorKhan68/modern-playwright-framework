55c112123cdab4c60917c050455fca90654e6ffe
Mansoor Khan <mansoorkhanlkl99@gmail.com>
2026-01-07 23:15:39 +0500

    fix: revert to hardcoded credentials in global setup for authentication

--- global-setup.js
+++ global-setup.js
@@ -10,13 +10,9 @@ export default async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   const auth = new Auth(page);
-  const userName = process.env.HRM_USERNAME;
-  const password = process.env.HRM_PASSWORD;
-  if (!userName || !password) {
-    console.warn(
-      'Credentials not available (PR pipeline). Skipping auth setup.'
-    );
-  }
+  const userName = 'Admin';
+  const password = 'admin123';
+  
   await auth.gotoLoginPage()
   await auth.userLogin(userName, password);
   await auth.verifyDashboardVisible();
